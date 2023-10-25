cee3cbb7a1b6c30e932a8b1e15a9870961285521
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-10-25 16:37:18 -0300

    feat: update

--- src/components/vm/VmInitializer.tsx
+++ src/components/vm/VmInitializer.tsx
@@ -1,16 +1,8 @@
 import { sanitizeUrl } from '@braintree/sanitize-url';
-import { setupKeypom } from '@keypom/selector';
 import { setupWalletSelector } from '@near-wallet-selector/core';
-import { setupHereWallet } from '@near-wallet-selector/here-wallet';
-import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 import type { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
 import { setupModal } from '@near-wallet-selector/modal-ui';
-import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 import { setupNearWallet } from '@near-wallet-selector/near-wallet';
-import { setupNeth } from '@near-wallet-selector/neth';
-import { setupNightly } from '@near-wallet-selector/nightly';
-import { setupSender } from '@near-wallet-selector/sender';
-import { setupWelldoneWallet } from '@near-wallet-selector/welldone-wallet';
 import Big from 'big.js';
 import {
   CommitButton,
@@ -31,7 +23,6 @@ import { setupFastAuth } from '@/lib/selector/setup';
 import { useAuthStore } from '@/stores/auth';
 import { useVmStore } from '@/stores/vm';
 import { networkId, signInContractId } from '@/utils/config';
-import { KEYPOM_OPTIONS } from '@/utils/keypom-options';
 
 export default function VmInitializer() {
   const [signedIn, setSignedIn] = useState(false);
@@ -57,16 +48,6 @@ export default function VmInitializer() {
           network: networkId,
           modules: [
             setupNearWallet(),
-            setupMyNearWallet(),
-            setupSender(),
-            setupHereWallet(),
-            setupMeteorWallet(),
-            setupNeth({
-              gas: '300000000000000',
-              bundle: false,
-            }),
-            setupNightly(),
-            setupWelldoneWallet(),
             setupFastAuth({
               networkId,
               signInContractId,
@@ -75,23 +56,6 @@ export default function VmInitializer() {
                   ? 'http://34.70.226.83:3030/relay'
                   : 'https://near-relayer-mainnet.api.pagoda.co/relay',
             }) as any, // TODO: Refactor setupFastAuth() to TS
-            setupKeypom({
-              trialAccountSpecs: {
-                url:
-                  networkId == 'testnet'
-                    ? 'https://test.near.org/#trial-url/ACCOUNT_ID/SECRET_KEY'
-                    : 'https://near.org/#trial-url/ACCOUNT_ID/SECRET_KEY',
-                modalOptions: KEYPOM_OPTIONS(networkId),
-              },
-              instantSignInSpecs: {
-                url:
-                  networkId == 'testnet'
-                    ? 'https://test.near.org/#instant-url/ACCOUNT_ID/SECRET_KEY/MODULE_ID'
-                    : 'https://near.org/#instant-url/ACCOUNT_ID/SECRET_KEY/MODULE_ID',
-              },
-              networkId,
-              signInContractId,
-            }) as any, // TODO: Refactor setupKeypom() to TS
           ],
         }),
         customElements: {
