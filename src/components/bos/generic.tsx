4fe0faae545772168cc0636c3ae61c6629278105
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-11-05 00:33:26 -0300

    feat: final

--- src/components/bos/generic.tsx
+++ src/components/bos/generic.tsx
@@ -4,7 +4,7 @@ export function GenericBOS(props: any) {
   return (
     <div
       className={props.className + ' relative w-max h-max bos'}
-      style={{ overflow: 'hidden' }}
+      style={{ overflow: 'hidden', maxHeight: '100%' }}
     >
       <VmComponent src={props.src} props={props.componentProps} />
     </div>
